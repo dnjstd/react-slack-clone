6f94fd52ed5271d485f3938f63d5777f870f1931
dnjstd <dnjstd@gmail.com>
2022-03-19 01:21:22 +0900

    로그인 레이아웃 구현

--- slack/pages/LogIn/index.tsx
+++ slack/pages/LogIn/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Button, Error, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
+import React, { useState } from 'react';
 
 const LogIn = () => {
-  return <div>로그인</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} />
+          </div>
+          {/* {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>} */}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <a href="/signup">회원가입 하러가기</a>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default LogIn;
