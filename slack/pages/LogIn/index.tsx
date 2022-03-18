6eec894c3e15edd4793d2f9827b46fb3829f8568
dnjstd <dnjstd@gmail.com>
2022-03-19 01:50:40 +0900

    swr로 로그인 사용자 정보 저장

--- slack/pages/LogIn/index.tsx
+++ slack/pages/LogIn/index.tsx
@@ -1,32 +1,59 @@
+import useInput from '@hooks/useInput';
 import { Button, Error, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
-import React, { useState } from 'react';
+import fetcher from '@utils/fetcher';
+import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+import useSWR from 'swr';
 
 const LogIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { data, error } = useSWR('/api/users', fetcher); //로그인된 사용자 정보를 전해줄 api
+  const [logInError, setLogInError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      setLogInError(false);
+      axios
+        .post('/api/users/login', { email, password })
+        .then(() => {})
+        .catch((error) => {
+          setLogInError(error.response?.data?.code === 401);
+        });
+    },
+    [email, password],
+  );
+
+  // console.log(error, userData);
+  // if (!error && userData) {
+  //   console.log('로그인됨', userData);
+  //   return <Redirect to="/workspace/sleact/channel/일반" />;
+  // }
 
   return (
     <div id="container">
       <Header>Sleact</Header>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Label id="email-label">
           <span>이메일 주소</span>
           <div>
-            <Input type="email" id="email" name="email" value={email} />
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
           </div>
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password" value={password} />
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
           </div>
-          {/* {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>} */}
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
         </Label>
         <Button type="submit">로그인</Button>
       </Form>
       <LinkContainer>
         아직 회원이 아니신가요?&nbsp;
-        <a href="/signup">회원가입 하러가기</a>
+        <Link to="/signup">회원가입 하러가기</Link>
       </LinkContainer>
     </div>
   );
