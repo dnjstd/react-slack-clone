9f566940e37d5d566557fa65737d6e23b11a8362
dnjstd <dnjstd@gmail.com>
2022-05-01 15:06:45 +0900

    Chat 박스 구현

--- slack/pages/DirectMessage/index.tsx
+++ slack/pages/DirectMessage/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Header } from '@pages/DirectMessage/styles';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
 import { useParams } from 'react-router-dom';
 import fetcher from '@utils/fetcher';
 import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR(`/api/users`, fetcher);
+  const [chat, onChangeChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+  }, []);
 
   if (!userData || !myData) {
     return null;
@@ -21,8 +28,8 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.name} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList/> */}
-      <ChatBox chat="" />
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
 };
