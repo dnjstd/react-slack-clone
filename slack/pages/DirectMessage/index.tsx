71e822f6e9e72dc4dfe96b2877ecbaad5fe15e24
dnjstd <dnjstd@gmail.com>
2022-05-03 01:41:03 +0900

    채팅 날짜별 분류 구현

--- slack/pages/DirectMessage/index.tsx
+++ slack/pages/DirectMessage/index.tsx
@@ -9,6 +9,7 @@ import ChatList from '@components/ChatList';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import { IDM } from '@typings/db';
+import makeSection from '@utils/makeSection';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
@@ -45,13 +46,15 @@ const DirectMessage = () => {
     return null;
   }
 
+  const chatSections = makeSection(chatData ? ([] as IDM[]).concat(...chatData).reverse() : []);
+
   return (
     <Container>
       <Header>
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.name} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatData={chatData} />
+      <ChatList chatSections={chatSections} />
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} />
     </Container>
   );
