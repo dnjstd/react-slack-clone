5dd2bb3777d1764a9f6652fe8661005b6cce46f8
dnjstd <dnjstd@gmail.com>
2022-05-03 01:11:27 +0900

    멘션 정규표현식 추가

--- slack/components/Chat/index.tsx
+++ slack/components/Chat/index.tsx
@@ -1,15 +1,34 @@
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
-import { IChat, IDM } from '@typings/db';
+import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 
-const Chat: VFC<Props> = ({ data }) => {
+const Chat: VFC<Props> = memo(({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          if (arr) {
+            return <Link to={`/workspace/${workspace}/dm/${arr[2]}`}>@{arr[1]}</Link>;
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
 
   return (
     <ChatWrapper>
@@ -21,10 +40,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
-};
+});
 
 export default Chat;
